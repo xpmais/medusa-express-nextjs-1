1ac61e9d743d96f786d31471f13e8d80f5400c3e
kitkatbr <91047095+kitkatbr@users.noreply.github.com>
2024-07-26 02:39:17 -0300

    update

--- src/components/payment/review.js
+++ src/components/payment/review.js
@@ -12,47 +12,33 @@ const Review = ({ cart }) => {
         alignItems: "top",
       }}
     >
-      
-      <Image
+      <Flex
+        sx={{
+          flex: 1,
+          flexDirection: "column",
+          fontWeight: "500",
+          fontSize: ".8em",
+          paddingLeft: "20px",
+        }}
+      >
+        <Text sx={{ fontWeight: 550, marginBottom: "10px" }}>{item.title}</Text>
+        <Flex
           sx={{
             width: "100%",
-            borderStartEndRadius: "10px",
-            borderStartStartRadius: "10px",
-            objectFit: "contain",
-            objectPosition: "center center",
-            clipPath: "polygon(0 0, 0 100%, 100% 75%, 100% 0%)",
-            boxShadow: "0px 0px 5px 2px rgba(0, 0, 0, 0.1)",
-
+            fontWeight: 300,
+            justifyContent: "space-between",
           }}
-          src={"/elden-ring.webp"}
-          alt={product.title}
-        />
-     
-      <Text
-        sx={{
-          mt: "16px",
-          lineHeight: "24px",
-          fontSize: "14px",
-          fontWeight: 300,
-          color: "#6B7280",
-        }}
-        variant="fz_s"
-      >
-    <div className="product-desc">
-      <span className="product-title">
-      {product.title} <b>{product.subtitle}</b>
-
-              <span className="badge">
-                New
-              </span>
-      </span>
-      <span className="product-caption">
-      {product.description}
-            </span>
-   
-      
-    </div>      </Text>
-
+        >
+          <Text sx={{ marginBottom: "15px" }}>
+            <Text sx={{ color: "#B0B0B0" }}>Platform: </Text>
+            {item.variant.title}
+          </Text>
+        </Flex>
+        <Text sx={{ fontWeight: 300 }}>
+          <Text sx={{ color: "#B0B0B0" }}>Quantity: </Text>
+          {item.quantity}
+        </Text>
+      </Flex>
     </Flex>
   )
 }
